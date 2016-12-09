6634da96f49185802e4497ae4dc6f3d5a46af41a
implicitdef <implicitdef@users.noreply.github.com>
2016-12-09 19:44:52 +0100

    removed dead code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::API
   include BaseController
-  before_action :require_feed_key!, only: [:secure]
 
   def hello
     render json: {key: Rails.application.secrets.feed_api_key}
   end
 
-  def secure
-    render json: {key: Rails.application.secrets.feed_api_key}
-  end
 
 end
 
